ab20d84ed1688a9a05294d783f28a68ec6e1d989
我是人 <463625542@qq.com>
2024-04-14 10:37:30 +0800

    my C code

---
+++ C语言代码/YanShiJiShuQi.c
@@ -0,0 +1,18 @@
+#include<stdint.h>
+int main()
+{
+    int i=1;
+    int a=0;
+    printf("(int=OK,9->=1)一一一一一\n\n");
+    while (1)
+    {
+        scanf("%d",&a);
+        if (a==9)
+        {
+            i=1;
+        }
+        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n%d\n",i);
+        i++;
+    }
+    return 0;
+} 
