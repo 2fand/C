6bb7ab1ffc7bcbd608e7885bdf2a424434a6d4f1
2fand <167662034+2fand@users.noreply.github.com>
2024-07-18 17:07:54 +0800

    Update Cmeiri.c

--- C每日/Cmeiri.c
+++ C每日/Cmeiri.c
@@ -2662,3 +2662,13 @@ int main() {
 	return 0;
 }
 *///超撒加密2.0(选自我的CSDN博客)^
+/*
+int main() {
+    int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+    int i = 0;
+    for (i = 0; i < 10; i++) {
+        printf("%d ", i[arr]);
+    }
+    return 0;
+}
+*///“i[arr]”^
